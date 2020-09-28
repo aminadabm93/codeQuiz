503654070a3c77f299bfdf2ba24bb87c67d0f38a
aminadabm93@gmail.com <aminadabm93@gmail.com>
2020-09-28 08:54:03 -0500

    added end screen, still needs functions to save data.

--- script.js
+++ script.js
@@ -18,31 +18,28 @@ var questions = ["Commonly used data types DO NOT include:","The condition in an
 var answerChoices = [["Strings","Booleans","Alerts","Numbers"],["Quotes","Curly brackets","Parentheses","Square Brackets"],
     ["Numbers and strings","Other arrays","Booleans","All of the above"],["Commas","Curly brackets","Quotes","Parentheses"],["JavaScript","Terminal/bash","for loops","console.log"]];
 
-    function startGame(){
-    //we must erase the start-screen div
-    //call the startQuestions function
-    
-}
-
 function startQuestions(){
-    //we must read question from array and print it to #questions
-    //add list & buttons w/ choices
-    //add listeners & see if they chose correct answer 
-    //print question
+    //clears start screen
     if(questionIndex==0){
         document.querySelector("#start-screen").innerHTML="";
         
     }
+    //clears previous answer choices
     else if(questionIndex>0){
         document.querySelector("#choices").innerHTML="";
     }
-    
+    //if at the last question
+    if(questionIndex==questions.length){
+        //end game screen
+        document.querySelector("#end-screen").style.visibility="visible";
+        document.querySelector("#questions").innerHTML="";
+        return null;
+    }
+    //create question 
     var questionTitle = document.querySelector("#question-title");
     questionTitle.textContent=questions[questionIndex];
-    
     //print answer choices
     for(var j =0; j<answerChoices[questionIndex].length;j++){
-        console.log("The question index is " +questionIndex);
         //create list item, and add button answer choices
         var list = document.createElement("li");
         document.querySelector("#choices").appendChild(list);
@@ -51,36 +48,26 @@ function startQuestions(){
         button.setAttribute("data-index",j);
         list.appendChild(button);
         list.appendChild(document.createElement("br"));
-        console.log("the button text is "+button.textContent);
     }
-
-    if(questionIndex==questions.length){
-        //end game screen
-        questionIndex=0;
-    }
-    else{
-        questionIndex++;
-    }
+    questionIndex++;
 }
 
 //this will evaluate answer right or wrong
 function nextQuestion(answerIndex){
     //update timer if wrong
-    console.log("User selected the button index #"+answerIndex);
+
     //move onto next question
     startQuestions();
 }
 
 
 startButton.addEventListener("click", startQuestions);
+document.querySelector("#end-screen").style.visibility="hidden";
 //this function will listen for the answer choice
-
 questionsDiv.addEventListener("click",function(event){
     if(event.target.matches("button")){
         var button = event.target;
         nextQuestion(button.getAttribute("data-index"));
-        //clear buttons
-        console.log("The tag has children: " +document.querySelector("#choices").hasChildNodes());
     }
 });
 
