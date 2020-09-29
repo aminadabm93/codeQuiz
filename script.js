c488bd20c898964afc262345972a72f342d3e368
aminadabm93@gmail.com <aminadabm93@gmail.com>
2020-09-28 19:24:52 -0500

    added timer and final score

--- script.js
+++ script.js
@@ -9,14 +9,18 @@
 //array for answer choices
 //array for correct answer choices 
 var questionIndex =0;
-var timer = 75;
+var finalScore=0;
+var secondsLeft = 75;
 var startButton = document.querySelector("#startButton");
 var questionsDiv = document.querySelector("#questions");
 var questions = ["Commonly used data types DO NOT include:","The condition in an if/else statement is enclosed within ___.",
-    "Arrays in JavaScript can be used to store ___.", "String values must be enclosed withtin ___ when being assigned to variables",
+    "Arrays in JavaScript can be used to store ___.", "String values must be enclosed within ___ when being assigned to variables",
     "A very useful tool used during development and debugging for printing content to the debugger is:"];
+
 var answerChoices = [["Strings","Booleans","Alerts","Numbers"],["Quotes","Curly brackets","Parentheses","Square Brackets"],
     ["Numbers and strings","Other arrays","Booleans","All of the above"],["Commas","Curly brackets","Quotes","Parentheses"],["JavaScript","Terminal/bash","for loops","console.log"]];
+
+var key = [2,2,3,2,3];
 
 function startQuestions(){
     //clears start screen
@@ -30,8 +34,11 @@ function startQuestions(){
     }
     //if at the last question
     if(questionIndex==questions.length){
-        //end game screen
+        //end game
+        finalScore=secondsLeft;
+
         document.querySelector("#end-screen").style.visibility="visible";
+        document.querySelector("#final-score").textContent=finalScore;
         document.querySelector("#questions").innerHTML="";
         return null;
     }
@@ -49,20 +56,36 @@ function startQuestions(){
         list.appendChild(button);
         list.appendChild(document.createElement("br"));
     }
-    questionIndex++;
 }
 
 //this will evaluate answer right or wrong
 function nextQuestion(answerIndex){
     //update timer if wrong
-
+    if(answerIndex!=key[questionIndex]){
+        secondsLeft-=10;
+        
+    }
+    //insert horizontal line and whether they got the question right or wrong
+    //questionsDiv.appendChild(document.createElement)
     //move onto next question
+    questionIndex++;
     startQuestions();
 }
 
-
+function startTime(){
+    var timer = document.querySelector("#timer");
+    var timerInterval = setInterval(function(){
+        timer.textContent="Time: "+secondsLeft;
+        secondsLeft--;
+    },1000);
+}
+//start the quiz
 startButton.addEventListener("click", startQuestions);
+startTime();
+
+questionsDiv.appendChild(document.createElement("hr"));
 document.querySelector("#end-screen").style.visibility="hidden";
+
 //this function will listen for the answer choice
 questionsDiv.addEventListener("click",function(event){
     if(event.target.matches("button")){
